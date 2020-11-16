9f0a55fc1bbf5350afc1ad0c6803e17a0387cc68
work-dev-br <workdevbr@gmail.com>
2020-11-15 22:04:30 -0300

    Commit 15-11-2020 22:04

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './content/home/home.component'
+import { ContactComponent } from './page/contact/contact.component'
+import { AboutComponent } from './page/about/about.component'
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component'
+
+const routes: Routes = [
+    { path: '',   redirectTo: '/home', pathMatch: 'full' }, 
+    { path: 'home', component: HomeComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '**', component: PageNotFoundComponent }, 
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { HeaderComponent } from './page/header/header.component';
+import { FooterComponent } from './page/footer/footer.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { AboutComponent } from './page/about/about.component';
+import { HomeComponent } from './content/home/home.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    AboutComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
